0cfd36a1a05ad16a2f43dbb192bc05db27ede020
ovt00 <ovt00@ukr.net>
2017-05-30 00:34:23 +0300

    Update __init__.py
    added the module .wr740n_

--- roscraco/router/tplink/__init__.py
+++ roscraco/router/tplink/__init__.py
@@ -3,6 +3,7 @@ from .wr340g import Tplink_WR340G, Tplink_WR340Gv4
 from .wr720n import Tplink_WR720N
 
 from .wr740n import Tplink_WR740N
+from .wr740n_ import Tplink_WR740N_
 from .wr741n import Tplink_WR741N
 
 from .wr940n import Tplink_WR940N
